5f64a9beb852e8075d60b255fc4174c52b001f5f
Surgetin <65120855+Surgetin@users.noreply.github.com>
2022-10-26 22:08:38 +0200

    added info container

---
+++ src/components/InfoContainer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const MapSection = () => {
+    return (
+        <div className="info_container">
+            <div className="info_content">
+                <ul className="info_lists">
+                    <li>IP Address</li>
+                    <li>Location</li>
+                    <li>Timezone</li>
+                    <li>ISP</li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default MapSection
