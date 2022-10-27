afdf7fe730231cd54fa962240d9d6b1cbe3d82c2
Surgetin <65120855+Surgetin@users.noreply.github.com>
2022-10-27 13:33:26 +0200

    added icon

--- src/components/MapSection.jsx
+++ src/components/MapSection.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
-import "leaflet/dist/leaflet.css"
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import "leaflet/dist/leaflet.css";
+import icon from "./icon"
 
 const position = [51.505, -0.09]
 
@@ -12,7 +13,7 @@ export const MapSection = () => {
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                <Marker position={position}>
+                <Marker icon={icon} position={position}>
                     <Popup>
                         A pretty CSS3 popup. <br /> Easily customizable.
                     </Popup>
